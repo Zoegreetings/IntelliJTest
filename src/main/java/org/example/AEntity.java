0daa36c40f606add561aff57f8f7409c6a07ff09
pingyingzeng <zenggreetings@gamil.com>
2022-12-05 11:35:34 -0500

    commit 1

---
+++ src/main/java/org/example/AEntity.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class AEntity {
+    private int id;
+    private String name;
+
+    public AEntity(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
